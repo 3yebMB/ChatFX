25f767f4a1e15bf28082a91a946144a751e38c32
3yebmb <3yebmb@gmail.com>
2019-01-13 19:08:12 +0500

    show setting window

--- src/sample/controllers/Controller.java
+++ src/sample/controllers/Controller.java
@@ -1,12 +1,17 @@
 package sample.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -29,8 +34,27 @@ public class Controller {
     @FXML
     Button btn;
 
+    @FXML
+    Button settingButton;
+
     DateFormat dateFormat = new SimpleDateFormat("dd.MM.yy HH:mm:ss ");
     String path = "..\\log\\2019-01-12.html";
+
+    public void setSettingButton(){
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/sample/view/sett.fxml"));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
+    }
 
     public void sendMsg() {
 
